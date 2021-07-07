e197a5317b41e751b5eaa62713eb2ecaacead452
dienigma <joshi.chinmay09@gmail.com>
2021-07-07 22:58:56 +0530

    Implement CK Editor V 5

---
+++ src/CKEditor5.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { CKEditor } from '@ckeditor/ckeditor5-react';
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+const CKEditor5 = ({ data }) => {
+  return (
+    <div className="editor-wrapper">
+      <h3>CKEditor 5</h3>
+      <CKEditor editor={ClassicEditor} data={data} />
+    </div>
+  );
+};
+
+export default CKEditor5;
